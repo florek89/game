bb80a143706b78577b9a59b43946a1c577c3d95e
Piotr Florys <piotr.florys@gmail.com>
2017-04-27 11:36:34 +0200

    male zmiany

--- game.py
+++ game.py
@@ -7,15 +7,20 @@ def create_board(width,height):
    for column in range(height):
        main_board.append(inner_board[:])
 
+
    for row in range(1, height - 1):
-       main_board[row] = ["x" + int(width-2) * " " + "#"]
+       main_board[row] = ["#" + int(width-2) * " " + "#"]
 
-   return main_board
+       return main_board
 
-def print_board(board):
+# def point (z):
+#     z = main_board.insert(150, "R")
+#     return z
+
+def print_board (board):
    for line in board:
        print(*line)
-
+    #    print (x)
 
 def main():
    print_board(create_board(143,42))
