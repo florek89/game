b4f293d96289fb99d51ad2e56f0b4467ff612530
Piotr Florys <piotr.florys@gmail.com>
2017-04-27 15:59:39 +0200

    Change def move

--- game.py
+++ game.py
@@ -1,27 +1,29 @@
-width = 50
-height = 10
-
+import os
 
 def create_board(width, height):
-   height_list = []
-   width_list = []
-   width2 = []
-   for row in range(width):
-       width_list.append('X')
-   for row in range(height - 2):
-       height_first = []
-       height_list.append(height_first)
-       for row in range(width):
-           if row == 0 or row == width - 1 :
-               height_first.append('X')
-           else:
-               height_first.append(' ')
+    '''This function creates board with parametrs: width and height'''
+    board = []
+    upper_board = []
+    lower_board = []
+    frame_board = []
 
-   for el in range(width):
-       width2.append('X')
-   board = [width_list, *height_list, width2]
-   return board
+    for row in range(width):
+        upper_board.append("X")
+    board.append(upper_board)
+    lower_board = upper_board
 
+    for row in range(width):
+        if row == 0 or row == height-1:
+            frame_board.append("X")
+        else:
+            frame_board.append("-")
+
+    for i in range(height-2):
+        board.append(frame_board[:])
+
+    board.append(lower_board)
+
+    return board
 
 
 def print_board(board):
@@ -31,8 +33,6 @@ def print_board(board):
 def insert_player(board, x, y):
     board[x][y] = "@"
 
-def insert_player2(board, x, y):
-    board[x][y] = "@"
 
 def getch():
     import sys, tty, termios
@@ -45,13 +45,30 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return char
 
-def main():
-    complete_board = create_board(width,height)
-    insert_player(complete_board,2,2)
-    #insert_player2(complete_board,2,2)
-    char = getch()
-    if char=="w":
+def move():
+
+    if char == "w":
         insert_player(complete_board,1,2)
     print_board(complete_board)
+    if char == "a":
+        insert_player(complete_board, 2, 1)
+    print_board(complete_board)
+    if char == "s":
+        insert_player(complete_board,3,2)
+    print_board(complete_board)
+    if char == "d":
+        insert_player(complete_board, 2, 3)
+    print_board(complete_board)
+    # else:
+    #     print("Use 'W-S-A-D'")
+
+
+def main():
+    complete_board = create_board(10,10)
+    insert_player(complete_board,2,2)
+    char = getch()
+     = move()
+
+
 
 main()
