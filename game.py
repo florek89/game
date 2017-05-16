ec53fb2329361d04012958992d966c135d8109a4
Rafał Grochowski <wiliam1arafal@gmail.com>
2017-05-16 05:46:16 +0200

    New version fille v 2.0 for files

--- game.py
+++ game.py
@@ -1,48 +1,49 @@
 import os
+import csv
+import sys
 
-width = 60
-height = 40
 
-
-def create_board(width, height):
-    height_list = []
-    width_list = []
-    width_end = []
-    for poz in range(width):
-       width_list.append('X')
-    for poz in range(height - 2):
-       height_first = []
-       height_list.append(height_first)
-       for poz in range(width):
-           if poz == 0 or poz == width - 1 :
-               height_first.append('X')
-           else:
-               height_first.append(' ')
-
-    for poz in range(width):
-       width_end.append('X')
-    board = [width_list, *height_list, width_end]
+def create_board():
+    board = []
+    text = open("mapa.csv").read()
+    #print (text)
+    board.append([])
+    row = 0
+    for i in text:
+        if i == "\n":
+            board.append([])
+            row += 1
+        else:
+            board[row].append(i)
     return board
 
 
 def print_board(board):
-    for poz in board:
-        print(*poz)
+    for row in board:
+        print("".join(row))
 
-def tree(board, x, y):
+def text_insert (board, y, x, text):
+    variable=0
+    for variable in range(len(text)):
+        board[x][y+variable+1] = text[variable]
+    return board
+
+def tree(board, y, x , leaf="#"):
     board[x][y] = "&"
     board[x-1][y]= "&"
     board[x-2][y]= "&"
-    board[x-3][y-1]= "#"
-    board[x-3][y+1]= "#"
-    board[x-4][y-2]= "#"
-    board[x-4][y+2]= "#"
-    board[x-5][y-2]= "#"
-    board[x-5][y+2]= "#"
-    board[x-6][y-1]= "#"
-    board[x-6][y+1]= "#"
-    board[x-7][y]= "#"
+    board[x-3][y-1]= leaf
+    board[x-3][y+1]= leaf
+    board[x-4][y-2]= leaf
+    board[x-4][y+2]= leaf
+    board[x-5][y-2]= leaf
+    board[x-5][y+2]= leaf
+    board[x-6][y-1]= leaf
+    board[x-6][y+1]= leaf
+    board[x-7][y]= leaf
     return board
+
+
 
 def player(board, x, y):
     board[x][y] = "@"
@@ -59,38 +60,47 @@ def getch():
     print(char)
     return char
 
-
-def move():
-    char = getch()
-    getch()
-    if char == "w":
-        os.system("cls")
-        board = create_board(width, height)
-        player(board, 2, 1)
-        print_board(board)
-        move ()
-    if char == "a":
-        os.system("cls")
-        player(board, 2, 1)
-        print_board(board)
-    if char == "s":
-        os.system("cls")
-        player(board,3,2)
-        print_board(board)
-    if char == "d":
-        os.system("cls")
-        player(board, 2, 3)
-        print_board(board)
-    #     print("Use 'W-S-A-D'")
-
-
 def main():
-     board = create_board(width, height)
-     player(board, 1, 1)
-     tree(board, 13,13)
-     tree(board, 13,4)
-     tree(board, 15,20)
-     print_board(board)
-     move ()
+    x_player = 10 #starting position of player
+    y_player = 5
+    board = create_board()
+    player(board, y_player, x_player)
+    #tree(board,14,15,"*")
+    print_board(board)
+    x = getch()
+    while x != "q":
+
+       char = getch()
+       if char == "d":
+           if board[y_player][x_player + 1] != "X":
+               x_player += 1
+       elif char == "a":
+           if board[y_player][x_player - 1] != "X":
+               x_player -= 1
+       elif char == "s":
+           if board[y_player + 1][x_player]: #!= "X"
+               y_player += 1
+       elif char == "w":
+           if board[y_player - 1][x_player] != "X":
+               y_player -= 1
+       elif char == "q":
+           sys.exit()
+
+       board = create_board()
+       #mage(board,10,10)
+
+       text_insert(board,83,32,'Mana:')
+       text_insert(board,83,34,'Range:')
+       text_insert(board,83,36,'Skil:')
+       text_insert(board,100,32,'Wisdom:')
+       text_insert(board,100,34,'Atack:')
+       text_insert(board,100,36,'Deff:')
+       tree(board,15,11)
+       tree(board,31,20)
+       tree(board,40,21, '?')
+       tree(board,61,20,'?')
+       player(board, y_player, x_player)
+       print_board(board)
+       print(x_player,y_player)
 
 main()
